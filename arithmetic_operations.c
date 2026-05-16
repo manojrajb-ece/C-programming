d567bc386f49a8222d7741f166880fbd35786199
manojrajb-ece <manojrajb481@gmail.com>
2026-05-16 19:29:50 +0530

    Add arithmetic operations in C program
    Simple C program to perform arithmetic operations using float values.

---
+++ arithmetic_operations.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("Enter the numbers for arthematic opertion\n");
+    printf("Enter the first number: \n");
+    scanf("%f",&a);
+    printf("Enter the second number: \n");
+    scanf("%f",&b);
+    printf("Addition of two numbers is: %f\n",a+b);
+    printf("Subtraction of two numbers is: %f\n",a-b);
+    printf("Division of two numbers is: %f\n",a/b);
+    printf("Multiplication of two numbers is: %f\n",a*b);
+return 0;
+    
+    
+}
